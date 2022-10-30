c4c00ecdacd76685b16757f0b50a87a20d518df0
Jerry-Young-23 <102022333+Jerry-Young-23@users.noreply.github.com>
2022-10-30 20:02:34 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -5,6 +5,6 @@ apt update -y; apt install curl -y
 apt install socat;curl https://get.acme.sh | sh -s email=123456@qq.com
 source ~/.bashrc
 read -p "输入你的域名：" domain
-acme.sh --issue --standalone -d $domain
+acme.sh --issue --standalone -d $domain || acme.sh --issue --standalone -d $domain --server letsencrypt
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
 
