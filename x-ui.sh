6450a8d5983b829fad82bc37884a746091d1460f
Jerry-Young-23 <102022333+Jerry-Young-23@users.noreply.github.com>
2022-10-30 17:44:42 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -5,7 +5,7 @@ sed -i 's/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/' /etc/
 apt update -y; apt install curl -y
 apt install socat;curl https://get.acme.sh | sh -s email=my@example.com
 source ~/.bashrc
-read -p "输入你的域名" domain
+read -p "输入你的域名：" domain
 acme.sh --issue --standalone -d $domain
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
 
