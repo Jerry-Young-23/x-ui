b5fa8b1cfa3b3eb438993246a9a2fad7379d70f9
Jerry-Young-23 <102022333+Jerry-Young-23@users.noreply.github.com>
2022-10-30 17:31:29 +0800

    Create x-ui.sh

---
+++ x-ui.sh
@@ -0,0 +1,10 @@
+#!/bin/bash  
+
+timedatectl set-timezone Asia/Shanghai
+sed -i 's/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/' /etc/gai.conf  
+apt apdate -y; apt install curl -y
+apt install socat;curl https://get.acme.sh | sh -s email=my@example.com;source ~/.bashrc
+read -p "输入你的域名" domain
+acme.sh --issue --standalone -d $domain
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+
