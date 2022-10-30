30a348be36e1104fe4f8f02e4834490b45df2ac0
Jerry-Young-23 <102022333+Jerry-Young-23@users.noreply.github.com>
2022-10-30 19:53:00 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -2,7 +2,7 @@
 timedatectl set-timezone Asia/Shanghai
 sed -i 's/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/' /etc/gai.conf  
 apt update -y; apt install curl -y
-apt install socat;curl https://get.acme.sh | sh -s email=my@example.com
+apt install socat;curl https://get.acme.sh | sh -s email=123456@qq.com
 source ~/.bashrc
 read -p "输入你的域名：" domain
 acme.sh --issue --standalone -d $domain
